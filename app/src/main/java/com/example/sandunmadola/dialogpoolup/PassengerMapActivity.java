e998cb609889200f902e961dfd760ecc0a091a59
Sandun Priyamewan Madola <sadunmadola7@gmail.com>
2014-11-18 03:12:36 +0530

    Add Text Field to map(when type a location map load that place and view marker), Set Current location

--- app/src/main/java/com/example/sandunmadola/dialogpoolup/PassengerMapActivity.java
+++ app/src/main/java/com/example/sandunmadola/dialogpoolup/PassengerMapActivity.java
@@ -1,58 +1,134 @@
 package com.example.sandunmadola.dialogpoolup;
 
-import android.app.Activity;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
+import android.app.Dialog;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- * Created by Sandun_Madola on 9/23/2014.
- */
+import java.io.IOException;
+import java.util.List;
+
 public class PassengerMapActivity extends FragmentActivity {
-    private GoogleMap googleMap;
+
+    private static final int GPS_ERRORDIALOG_REQUEST = 9001;
+    public static final double SL_lat = 7.8775394;
+    public static final double SL_lng = 80.7003428;
+    private static final float DEFAULTZOOM = 7;
+    private static final float DEFAULTZOOM1 = 15;
+
+    GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_passengermap);
-        setUpMapIfNeeded();
 
-    }
+        if(servicesOK()){
 
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (googleMap == null) {
-            googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-            // Check if we were successful in obtaining the map.
-            if (googleMap != null) {
-                setUpMap();
-                // The Map is verified. It is now safe to manipulate the map.
+            setContentView(R.layout.activity_passengermap);
 
+            if(initMap()){
+                Toast.makeText(this,"Ready to Map", Toast.LENGTH_SHORT).show();
+                gotoLocation(SL_lat,SL_lng,DEFAULTZOOM);
+                mMap.setMyLocationEnabled(true);
             }
+            else{
+                Toast.makeText(this,"Map not available", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            setContentView(R.layout.activity_mode);
         }
     }
 
-    private void setUpMap() {
-        googleMap.setMyLocationEnabled(true);
+    public boolean servicesOK(){
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if(isAvailable == ConnectionResult.SUCCESS){
+            return true;
+        }
+        else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)){
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable,this,GPS_ERRORDIALOG_REQUEST);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this,"Can't connect to Google Play Services", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
 
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String provider = locationManager.getBestProvider(criteria,true);
-        Location location = locationManager.getLastKnownLocation(provider);
+    private boolean initMap(){
+        if (mMap == null){
+            SupportMapFragment mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map1);
+            mMap = mapFrag.getMap();
+        }
+        return (mMap != null);
+    }
 
-        double latitude =  location.getLatitude();
-        double longitude = location.getLongitude();
+    private void gotoLocation(double lat, double lng, float zoom){
+        LatLng ll = new LatLng(lat,lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll,zoom);
+        mMap.moveCamera(update);//animate camera
+    }
+    public void gotoGeoLocate(View v) throws IOException{
+        hideSoftKeyboard(v);
+        EditText et = (EditText) findViewById(R.id.txtPlace1);
+        String location = et.getText().toString();
 
-        LatLng latLng = new LatLng(latitude,longitude);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        googleMap.animateCamera(CameraUpdateFactory.zoomTo(20));
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude,longitude)).title("Me"));
+        Geocoder gc = new Geocoder(this);
+        List<Address> list = gc.getFromLocationName(location,1);
+        Address add = list.get(0);
+        String locality = add.getLocality();
+
+        Toast.makeText(this,locality,Toast.LENGTH_LONG).show();
+
+        double lat = add.getLatitude();
+        double lng = add.getLongitude();
+
+        gotoLocation(lat,lng,DEFAULTZOOM1);
+
+        MarkerOptions options = new MarkerOptions().title(locality).position(new LatLng(lat,lng));
+        mMap.addMarker(options);
+
+    }
+
+    private void hideSoftKeyboard(View v) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(),0);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mapTypeNormal:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.mapTypeSatellite:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.mapTypeTerrain:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+            case R.id.mapTypeHybrid:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+            case R.id.mapTypeNone:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
